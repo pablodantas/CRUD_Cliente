1cbc8e6b97b00b44125fdb24eb8cd1fd89c181c0
Pablo Dantas <pablodesouzadantas@gmail.com>
2021-12-26 18:18:17 -0300

    Projeto Trilha 1

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Maria Silva', '15555515151',5500.0,TIMESTAMP WITH TIME ZONE '1999-09-22T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Pablo Dantas', '12345678901',20000.0,TIMESTAMP WITH TIME ZONE '2000-02-07T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Sandra Souza', '112345654554',1255.5,TIMESTAMP WITH TIME ZONE '1976-03-06T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Patrick Bezerra', '1234123456',2505.5,TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('João Vitor', '123456712345',7000.0,TIMESTAMP WITH TIME ZONE '1990-12-20T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children ) VALUES ('Carlos almeida', '12345678901',1952.0,TIMESTAMP WITH TIME ZONE '1999-07-25T10:30:00Z', 5);
